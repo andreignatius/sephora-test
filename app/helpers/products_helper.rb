2f8dbe9f2b771098fb09ed367ca8a1f50c7628a8
betajulian <andre.2121@yahoo.com>
2016-05-12 17:05:47 +0000

    added users and signup

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,6 @@
 module ProductsHelper
     # Returns the Gravatar for the given product
     def gravatar_for(product, options = { size: 50 })
-        #gravatar_id = Digest::MD5::hexdigest(Faker::Internet.email.strip.downcase)
-        #gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-        #image_tag(gravatar_url, alt: product.name, class: "gravatar")
         image_tag(Faker::Company.logo, :width => 100, :height => 100)
     end
 end
