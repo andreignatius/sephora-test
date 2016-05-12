61e5499965f97559c2b95168a1227d45045230a8
betajulian <andre.2121@yahoo.com>
2016-05-12 18:25:42 +0000

    added users and signup

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,6 @@
 module ProductsHelper
     # Returns the Gravatar for the given product
-    def gravatar_for(product, options = { size: 50 })
+    def gravatar_for_product(product, options = { size: 50 })
         image_tag(Faker::Company.logo, :width => 100, :height => 100)
     end
 end
