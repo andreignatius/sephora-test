f2187c2d8115295561d583abad71cc7e4d8ab18c
betajulian <andre.2121@yahoo.com>
2016-05-17 16:18:26 +0000

    log in view for edit editted

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'help'      => 'static_pages#help'
   get 'about'     => 'static_pages#about'
   get 'contact'   => 'static_pages#contact'
-  get '/products/:id/edit', to: 'products#edit'
+  get '/products/:id', to: 'products#edit'
   get 'addproduct'=> 'products#new'
   get 'signup'    => 'users#new'
   get 'login'     => 'sessions#new'
